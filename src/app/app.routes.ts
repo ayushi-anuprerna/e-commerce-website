6e32883ff8457e9213c908d7e21368cd91c2df0b
Ayushi <ayushipathak0330@gmail.com>
2026-03-04 19:53:31 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { Login } from './component/login/login';
+import { Cart } from './component/cart/cart';
+import { ProductDetail } from './component/product-detail/product-detail';
+import { ProductList } from './component/product-list/product-list';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"",component:Home},
+    {path:"login",component:Login},
+    {path:"cart",component:Cart},
+    {path:"productDetail",component:ProductDetail},
+    {path:"productList",component:ProductList}
+
+
+];
